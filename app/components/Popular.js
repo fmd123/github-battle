b30ffd68e580b2c6c10394534c021b084ec09bd8
fmd123 <fmdavis6@gmail.com>
2017-11-22 21:36:05 -0700

    70% done

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,5 +1,36 @@
 var React = require('react');
 var PropTypes = require('prop-types');
+var api = require('../utils/api');
+
+function RepoGrid(props){
+  return(
+    <ul className = 'popular-list'>
+      {props.repos.map(function(repo, index){
+        return(
+        <li key={repo.name} className = 'popular-item'>
+          <div className = 'popular-rank'>#{index + 1}</div>
+          <ul className = 'space-list-items'>
+            <li>
+              <img
+                className = 'avatar'
+                src = {repo.owner.avatar_url}
+                alt = {'Avatar for ' + repo.owner.login}
+              />
+            </li>
+            <li><a href = {repo.html_url}>{repo.name}</a></li>
+            <li>@{repo.owner.login}</li>
+            <li>{repo.stargazers_count}stars</li>
+          </ul>
+        </li>
+      )
+      })}
+    </ul>
+  )
+}
+
+RepoGrid.propTypes = {
+  repos: PropTypes.array.isRequired,
+}
 
 function SelectLanguage(props){
   var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -31,6 +62,7 @@ class Popular extends React.Component{
     super(props);
     this.state = {
       selectedLanguage: 'All',
+      repos: null
     }
     this.updateLanguage = this.updateLanguage.bind(this);
     // above line is what keeps "this" always bound to correct context
@@ -38,12 +70,25 @@ class Popular extends React.Component{
     //here the context being taken in is the 'this' variable for whatever is being clicked.
   }
 
+  componentDidMount(){
+    this.updateLanguage(this.state.selectedLanguage)
+  }
+
   updateLanguage(lang){
     this.setState(function(){
       return{
-        selectedLanguage: lang
+        selectedLanguage: lang,
+        repos: null
       }
-    })
+    });
+    api.fetchPopularRepos(lang)
+    .then(function(repos){
+      this.setState(function(){
+        return{
+          repos:repos
+        }
+      });
+    }.bind(this))
   }
   render(){
 
@@ -53,6 +98,9 @@ class Popular extends React.Component{
           selectedLanguage = {this.state.selectedLanguage}
           onSelect = {this.updateLanguage}
         />
+        {!this.state.repos
+        ?<p>Loading</p>
+      :<RepoGrid repos = {this.state.repos}/>}
       </div>
     )
   }
